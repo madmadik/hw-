4143f813f8b32a88ad6126555156f2f7081e2649
madik <madik404error@gmail.com>
2017-12-11 22:30:50 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            int sum = 0;
+            int size  = 20;
+            Random randomize = new Random();
+            for(int i=0;i<size;i++)
+            {
+                list.Add(randomize.Next() % 100);
+                if(i%2==0)
+                {
+                    sum += list[i];
+                }
+            }
+            int max =list[0];
+            foreach (var numbers in list)
+            {
+                if(numbers>max)
+                {
+                    max = numbers;
+                }
+                Console.Write(numbers+"   ");
+            }
+            Console.WriteLine("\n");
+            int max2 = list[0];
+            foreach(var numbers in list)
+            {
+                if(numbers>max2&&numbers<max)
+                {
+                    max2 = numbers;
+                }
+            }
+            Console.WriteLine("max = "+max);
+            Console.WriteLine("max2 = " + max2);
+            Console.ReadLine();
+        }
+    }
+}
